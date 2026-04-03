145cb7cd9832eef7679c315b1b6935d74ab74c86
WildTom1 <40327566+WildTom1@users.noreply.github.com>
2026-04-03 14:16:05 -0500

    Adds config
    Allows for the user to change the location that the script looks for the videos.

--- playVid.sh
+++ playVid.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+FILELOCATION="/usr/share/playVid/"
+
+#lets the user set a different place for the videos (can be good for those with low file size or a lot of videos)
+if [ -f "/etc/playVid.conf" ]; then
+    FILELOCATION=$(cat /etc/playVid.conf)
+fi
+
 openbox &
 cd /usr/share/playVid/
 ls |sort -R |tail -$N |while read file; do
-    vlc --one-instance -f --no-osd -I dummy /usr/share/playVid/$file vlc://quit
+    vlc --one-instance -f --no-osd -I dummy $FILELOCATION/$file vlc://quit
     break
 done
 
